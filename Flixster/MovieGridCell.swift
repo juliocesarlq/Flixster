dc1af08deb181ab4e1cf2cf587652f05ae9d3008
Julio Cesar Lopez <lopezjuliocesar1994@gmail.com>
2021-05-25 02:26:37 -0700

    Part 2 Additions

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Julio C Lopez on 5/25/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
